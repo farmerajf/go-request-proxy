bb3c2a59dfda59e263d8b286caa174b3558f02b2
Adam Farmer <adam.farmer@safetyculture.io>
2020-10-24 00:31:45 +1100

    Get and validate queue for request

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 	"net/http"
 )
 
-var requests = []string{}
+var queues map[string][]string
 var logger simpleLogger
 
 func main() {
 	logger = &consoleLogger{}
+	queues = make(map[string][]string)
 
 	config, err := loadConfig()
 	if err != nil {
@@ -18,7 +19,7 @@ func main() {
 		return
 	}
 
-	fmt.Println(config)
+	initConfig(config)
 
 	http.HandleFunc("/request", handleRequest)
 	http.HandleFunc("/retrieve", handleRetrieve)
@@ -29,15 +30,33 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(r.Body)
 	body := buf.String()
+	queueName := r.URL.RawQuery
 
-	requests = append(requests, body)
+	logger.log("New request for queue " + queueName)
 
-	fmt.Fprintf(w, "OK")
+	queue, exists := queues[queueName]
+	if !exists {
+		w.WriteHeader(http.StatusNotFound)
+		logger.log("Queue " + queueName + " does not exist ")
+
+		return
+	}
+	queue = append(queue, body)
+	queues[queueName] = queue
+
+	logger.log("Added request to queue " + body)
 }
 
 func handleRetrieve(w http.ResponseWriter, _ *http.Request) {
-	r, nr := requests[0], requests[1:]
-	requests = nr
+	//r, nr := requests[0], requests[1:]
+	//requests = nr
 
-	fmt.Fprintf(w, r)
+	//fmt.Fprintf(w, r)
+}
+
+func initConfig(config *config) {
+	for queueName, _ := range config.Queues {
+		queues[queueName] = []string{}
+		logger.log("Initialised queue " + queueName)
+	}
 }
